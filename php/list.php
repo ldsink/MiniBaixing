859c24ccb1ff0a408390a69e2f12c57e5c8742a4
troymaple <6579598@qq.com>
2014-07-10 16:24:48 +0800

    tiao zhuan

--- php/list.php
+++ php/list.php
@@ -2,7 +2,6 @@
 	require "MyDB.php";
 	header("Content-Type: text/html; charset=utf-8");
 	$categoryId = $_GET['category'];
-	echo "$categoryId";
 	$listArray = MyDB::getList($categoryId);
 	// $listArray = array(	array("id", "标题","内容","价格","QQ"), 
 	// 					array("1","NokiaXXX跳楼价","X成新，水货，欲购从速","1200","129078923"),
@@ -17,7 +16,7 @@
 
 	// 输出标题
 	echo "<tr>";
-	for ($i = 0 ; $i < 4 && $i < count($arrayTitle); $i++ )
+	for ($i = 0 ; $i < 5 && $i < count($arrayTitle); $i++ )
 	{ 
 		printf("<td>%s</td>",$arrayTitle[$i]);
 	}
@@ -29,7 +28,7 @@
 		$arrayItem = $listArray[$j];
 		echo "<tr>";
 		$infoId = $arrayItem[0];
-		for ($i = 0 ; $i < 4 && $i < count($arrayItem); $i++ )
+		for ($i = 0 ; $i < 5 && $i < count($arrayItem); $i++ )
 		{ 
 			echo "<td>";
 			if($i == 1)
--- php/post.php
+++ php/post.php
@@ -1,11 +1,12 @@
 <?php
+	require "MyDB.php";
 	header("Content-Type: text/html; charset=utf-8");
 
 	$categoryId = $_GET['categoryId'];
 	printf ("新增%s<br>", $categoryId);
 
-	// $adAttr = getFields($categoryId);
-	$adAttr = array("标题","内容","价格","QQ");
+	$adAttr = MyDB::getFields($categoryId);
+	//$adAttr = array("标题","内容","价格","QQ");
 
 	printf("<form name=\"input\" action=\"sendpost.php?categoryId=%s\" method=\"post\">",$categoryId);
 	foreach ($adAttr as $key => $value){
